2eca8a1b7e919caa514b620b7ebbf3d33b071d4e
ABHAY PRATAP CHAUHAN <abhaychauhan2906@gmail.com>
2026-02-24 22:24:41 +0530

    justsome small work

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,13 +4,17 @@ import authRoutes from "./routes/auth.route.js"
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 import { userRoutes } from "./routes/user.route.js";
-import chatRoutes from "./routes/chat.route.js"
+import chatRoutes from "./routes/chat.route.js";
+import cors from "cors";
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT;
 
-
+app.use({
+  origin:"http://localhost:5173",
+  credentials:true, // allow frontend to send cookies
+})
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/auth",authRoutes);
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,40 @@
-import React from 'react'
-import HomePage from './pages/HomePage'
-import LoginPage from './pages/LoginPage'
-import SignUpPage from './pages/SignUpPage'
-import NotificationPage from './pages/NotificationPage'
-import CallPage from './pages/CallPage'
-import ChatPage from './pages/ChatPage'
-import OnBoardingPage from './pages/OnBoardingPage'
+import React from 'react';
+import { Route , Routes } from 'react-router';
+import HomePage from './pages/HomePage.jsx'
+import LoginPage from './pages/LoginPage.jsx'
+import SignUpPage from './pages/SignUpPage.jsx'
+import NotificationPage from './pages/NotificationPage.jsx'
+import CallPage from './pages/CallPage.jsx'
+import ChatPage from './pages/ChatPage.jsx'
+import OnBoardingPage from './pages/OnBoardingPage.jsx'
+import  {Toaster} from "react-hot-toast";
+import { BrowserRouter } from 'react-router';
+import {useQuery,QueryClient,QueryClientProvider,} from '@tanstack/react-query';
+import { axiosInstance } from './lib/axios.js';
 
+// use query when you want fetch some data you can do it using usestate + useref but it will be easy when you use useQuery
 const App = () => {
+  const {data , isLoading,error}=useQuery({
+    queryKey:["todos"],
+    queryfn: async ()=>{
+      const res=await axiosInstance.get("/auth/me");
+      return res.data;
+    }
+  })
   return (
     <div className=' h-screen' data-theme='night'>
+    
       <Routes>
         <Route path="/" element={<HomePage/>}/>
         <Route path="/login" element={<LoginPage/>}/>
         <Route path="/signup" element={<SignUpPage/>}/>
         <Route path="/notification" element={<NotificationPage/>}/>
-        <Route path="/callpage" element={<CallPage/>}/>
-        <Route path="/chatpage" element={<ChatPage/>}/>
+        <Route path="/call" element={<CallPage/>}/>
+        <Route path="/chat" element={<ChatPage/>}/>
         <Route path="/onboarding" element={<OnBoardingPage/>}/>
-
+      
       </Routes>
+      <Toaster/>
     </div>  
   )
 }

1e7a763fb93b77d219b9a7d6b1086a1a498bda2c
ABHAY PRATAP CHAUHAN <abhaychauhan2906@gmail.com>
2026-02-24 17:11:40 +0530

    frontend Routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,26 @@
 import React from 'react'
+import HomePage from './pages/HomePage'
+import LoginPage from './pages/LoginPage'
+import SignUpPage from './pages/SignUpPage'
+import NotificationPage from './pages/NotificationPage'
+import CallPage from './pages/CallPage'
+import ChatPage from './pages/ChatPage'
+import OnBoardingPage from './pages/OnBoardingPage'
 
 const App = () => {
   return (
-    <div className='bg-red-500 h-screen text-5xl' data-theme='forest'>
-      <button className="btn">Button</button>
-<button className="btn btn-neutral">Neutral</button>
-<button className="btn btn-primary">Primary</button>
-<button className="btn btn-secondary">Secondary</button>
-<button className="btn btn-accent">Accent</button>
-<button className="btn btn-ghost">Ghost</button>
-<button className="btn btn-link">Link</button>
-    </div>
+    <div className=' h-screen' data-theme='night'>
+      <Routes>
+        <Route path="/" element={<HomePage/>}/>
+        <Route path="/login" element={<LoginPage/>}/>
+        <Route path="/signup" element={<SignUpPage/>}/>
+        <Route path="/notification" element={<NotificationPage/>}/>
+        <Route path="/callpage" element={<CallPage/>}/>
+        <Route path="/chatpage" element={<ChatPage/>}/>
+        <Route path="/onboarding" element={<OnBoardingPage/>}/>
+
+      </Routes>
+    </div>  
   )
 }
 
---
+++ frontend/src/pages/NotificationPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NotificationPage = () => {
+  return (
+    <div>
+      Noti
+    </div>
+  )
+}
+
+export default NotificationPage

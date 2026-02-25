8a9c731a7d50deb37301a40ec471ce07f01404c2
ABHAY PRATAP CHAUHAN <abhaychauhan2906@gmail.com>
2026-02-25 17:25:31 +0530

    working on signup page

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { useState } from "react";
 import { ShipWheelIcon } from "lucide-react";
+import { Link } from "react-router";
+import {useMutation} from "@tanstack/react-query"
 const SignUpPage = () => {
   const [signupData, setSignupData] = useState({
     fullName: "",
     email: "",
     password: "",
   });
+  const {mutate , isPending, error}=useMutation();
+
   const handleSignUp = (e) => {
     e.preventDefault();
   };
@@ -17,7 +21,7 @@ const SignUpPage = () => {
     >
       <div className="border border-primary/25 flex flex-col lg:flex-row w-full max-w-5xl mx-auto bg-base-100 rounded-xl shadow-lg overflow-hidden">
         {/*Signup form -- left side */}
-        <div className="w-full lg:w1/2 p-4 sm:p-8 flex flex-col">
+        <div className="w-full lg:w-1/2 p-4 sm:p-8 flex flex-col">
           {/*LOGO*/}
           <div className="mb-4 flex items-center justify-start gap-2">
             <ShipWheelIcon className="size-9 text-primary" />
@@ -35,14 +39,120 @@ const SignUpPage = () => {
                   </p>
                 </div>
                 <div className="space-y-3">
+                  {/*FULL NAME */}
                   <div className="form-control w-full">
-                    <label className="label"><span className="label-text">Full Name</span>
+                    <label className="label">
+                      <span className="label-text">Full Name</span>
                     </label>
-                    <input type="text" placeholder="john Doe" className="input input-bordered w-full" value={signupData.fullName} onChange={(e)=>setSignupData({...signupData,fullName:e.target.value})} required/>
+                    <input
+                      type="text"
+                      placeholder="john Doe"
+                      className="input input-bordered w-full"
+                      value={signupData.fullName}
+                      onChange={(e) =>
+                        setSignupData({
+                          ...signupData,
+                          fullName: e.target.value,
+                        })
+                      }
+                      required
+                    />
                   </div>
+                  {/*Email */}
+                  <div className="form-control w-full">
+                    <label className="label">
+                      <span className="label-text">Email</span>
+                    </label>
+                    <input
+                      type="email"
+                      placeholder="johnDoe@gmail.com"
+                      className="input input-bordered w-full"
+                      value={signupData.email}
+                      onChange={(e) =>
+                        setSignupData({ ...signupData, email: e.target.value })
+                      }
+                      required
+                    />
+                  </div>
+                  <div className="form-control w-full">
+                    <label className="label">
+                      <span className="label-text">Password</span>
+                    </label>
+                    <input
+                      type="password"
+                      placeholder="*********"
+                      className="input input-bordered w-full"
+                      value={signupData.password}
+                      onChange={(e) =>
+                        setSignupData({
+                          ...signupData,
+                          password: e.target.value,
+                        })
+                      }
+                      required
+                    />
+                    <p className="text-xs opacity-70 mt-1">
+                      Password must be atleast 6 characters long
+                    </p>
+                  </div>
+                  <div className="form-control">
+                    <label className="label cursor-pointer justify-start gap-2">
+                      <input
+                        type="checkbox"
+                        className="checkbox checkbox-sm"
+                        required
+                      />
+                      <span className="text-xs leading-tight">
+                        {" "}
+                        i agree to the{" "}
+                        <span className="text-primary hover:underline">
+                          terms of service
+                        </span>{" "}
+                        and{" "}
+                        <span className="text-primary hover:underline">
+                          privacy policy
+                        </span>
+                      </span>
+                    </label>
+                  </div>
+                </div>
+                <button className="btn btn-primary w-full" type="submit">
+                  {" "}
+                  Create Account
+                </button>
+                <div className="text-center mt-4">
+                  <p className="text-sm">
+                    Already have an account?{" "}
+                    <Link to="/login" className="text-primary hover:underline">
+                      Sign In
+                    </Link>
+                  </p>
                 </div>
               </div>
             </form>
+          </div>
+        </div>
+        {/*SignUp Page right side */}
+        <div className="hidden lg:flex w-full lg:w-1/2 bg-primary/10 items-center justify-center">
+          <div className="max-w-md p-8">
+            {/*Illustration*/}
+            <div className="relative aspect-square max-w-sm mx-auto">
+              <img
+                src="/i.png"
+                alt="Language Connection illustration"
+                className="w-full h-full"
+              />
+            </div>
+            <div className="text-center space-y-3 mt-6">
+              <h2 className="text-xl font-semibold">
+                Connect with language partners worldwide
+              </h2>
+              <p className="opacity-70">
+                {" "}
+                Practice conversation , make friends and improve your language
+                skills together
+              </p>
+            </div>
           </div>
         </div>
       </div>

349b26ea06708b1ed02e8a4c2a89c6660586d4f2
ABHAY PRATAP CHAUHAN <abhaychauhan2906@gmail.com>
2026-02-25 07:23:11 +0530

    working with signup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route , Routes } from 'react-router';
+import { Navigate, Route , Routes } from 'react-router';
 import HomePage from './pages/HomePage.jsx'
 import LoginPage from './pages/LoginPage.jsx'
 import SignUpPage from './pages/SignUpPage.jsx'
@@ -14,24 +14,25 @@ import { axiosInstance } from './lib/axios.js';
 
 // use query when you want fetch some data you can do it using usestate + useref but it will be easy when you use useQuery
 const App = () => {
-  const {data , isLoading,error}=useQuery({
-    queryKey:["todos"],
+  const {data:authData , isLoading,error}=useQuery({
+    queryKey:["authUser"],
     queryfn: async ()=>{
       const res=await axiosInstance.get("/auth/me");
       return res.data;
     }
-  })
+  });
+  const authUser=authData?.user
   return (
     <div className=' h-screen' data-theme='night'>
     
       <Routes>
-        <Route path="/" element={<HomePage/>}/>
-        <Route path="/login" element={<LoginPage/>}/>
-        <Route path="/signup" element={<SignUpPage/>}/>
-        <Route path="/notification" element={<NotificationPage/>}/>
-        <Route path="/call" element={<CallPage/>}/>
-        <Route path="/chat" element={<ChatPage/>}/>
-        <Route path="/onboarding" element={<OnBoardingPage/>}/>
+        <Route path="/" element={authUser ? <HomePage/> : <Navigate to ="/login"/>}/>
+        <Route path="/login" element={!authUser ?<LoginPage/>: <Navigate to ="/"/>}/>
+        <Route path="/signup" element={!authUser?<SignUpPage/>: <Navigate to="/"/>}/>
+        <Route path="/notification" element={authUser?<NotificationPage/>:<Navigate to="/login"/>}/>
+        <Route path="/call" element={authUser?<CallPage/>:<Navigate to="/login"/>}/>
+        <Route path="/chat" element={authUser?<ChatPage/>:<Navigate to="/login"/>}/>
+        <Route path="/onboarding" element={authUser?<OnBoardingPage/> : <Navigate to="/login"/>}/>
       
       </Routes>
       <Toaster/>
--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,11 +1,53 @@
-import React from 'react'
-
+import React from "react";
+import { useState } from "react";
+import { ShipWheelIcon } from "lucide-react";
 const SignUpPage = () => {
+  const [signupData, setSignupData] = useState({
+    fullName: "",
+    email: "",
+    password: "",
+  });
+  const handleSignUp = (e) => {
+    e.preventDefault();
+  };
   return (
-    <div>
-      SignUpPage
+    <div
+      className="h-screen flex items-center justify-center p-4 sm:p-6 md:p-8"
+      data-theme="forest"
+    >
+      <div className="border border-primary/25 flex flex-col lg:flex-row w-full max-w-5xl mx-auto bg-base-100 rounded-xl shadow-lg overflow-hidden">
+        {/*Signup form -- left side */}
+        <div className="w-full lg:w1/2 p-4 sm:p-8 flex flex-col">
+          {/*LOGO*/}
+          <div className="mb-4 flex items-center justify-start gap-2">
+            <ShipWheelIcon className="size-9 text-primary" />
+            <span className="text-3xl font-bold font-mono bg-clip-text text-transparent bg-gradient-to-r from-primary to-secondary tracking-wider">
+              Streamfiy
+            </span>
+          </div>
+          <div className="w-full">
+            <form onSubmit={handleSignUp}>
+              <div className="space-y-4">
+                <div>
+                  <h2 className="text-xl font-semibold">Create an Account</h2>
+                  <p className="text-sm opacity-70">
+                    Join Streamify and start your language learning adventure!
+                  </p>
+                </div>
+                <div className="space-y-3">
+                  <div className="form-control w-full">
+                    <label className="label"><span className="label-text">Full Name</span>
+                    </label>
+                    <input type="text" placeholder="john Doe" className="input input-bordered w-full" value={signupData.fullName} onChange={(e)=>setSignupData({...signupData,fullName:e.target.value})} required/>
+                  </div>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default SignUpPage
+export default SignUpPage;

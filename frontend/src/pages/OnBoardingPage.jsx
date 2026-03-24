ba29c2378496e72d51784d0a711cece24bbd44c3
ABHAY PRATAP CHAUHAN <abhaychauhan2906@gmail.com>
2026-03-24 16:30:30 +0530

    avatar work

--- frontend/src/pages/OnBoardingPage.jsx
+++ frontend/src/pages/OnBoardingPage.jsx
@@ -1,9 +1,10 @@
 import { QueryClient, useMutation } from "@tanstack/react-query";
 import useAuthUser from "../hooks/useAuthUser"
-import toast from "react-hot-toast";
+import toast, { LoaderIcon } from "react-hot-toast";
 import { useState } from "react";
 import { completeOnboarding } from "../lib/api.js";
-import { CameraIcon } from "lucide-react";
+import { CameraIcon, MapPinIcon, ShipWheelIcon, ShuffleIcon } from "lucide-react";
+import { LANGUAGES } from "../constants/index.js";
 
 const OnBoardingPage = () => {
    const {authUser}=useAuthUser();
@@ -27,8 +28,13 @@ const OnBoardingPage = () => {
   e.preventDefault();
   onboardingMutation(formState);
  }
-                  console.log(authUser);
-                console.log(formState.profilePic);
+ const handleSubmitAvatar =(e)=>{
+  const idx=Math.floor(Math.random()*100) + 1;//1-100 included
+  const randomAvatar=`https://avatar.iran.liara.run/public/${idx}.png`;
+  setFormState({...formState,profilePic:randomAvatar});
+  toast.success("Random profile picture generated!")
+ }
+                  console.log("RENDER:", formState.profilePic);
    return (
     <div className="min-h-screen bg-base-100 flex items-center justify-center p-4">
       <div className="card bg-base-200 w-full max-w-3xl shadow-xl">
@@ -39,19 +45,108 @@ const OnBoardingPage = () => {
             <div className="flex flex-col items-center justify-center space-y-4">
               {/**image preview*/}
               <div className="w-32 h-32 rounded-full bg-base-300 overflow-hidden">
-                {formState.profilePic?(
+                {formState.profilePic&&(
                 <img src={formState.profilePic} alt="profile Preview"
-                className="w-full h-full object-cover"/>):(
+                className="w-full h-full object-cover"/>)}
+                {!formState.profilePic&&(
                   <div className="flex items-center justify-center h-full">
                     <CameraIcon className="size-12 text-base-content opacity-40"/>
 
                   </div>
                 )}
 
-              </div>
 
+              </div>
+            {/* Generate Random Avatar BTN*/}
+            <div className="flex items-center justify-center space-y-4">
+              <button type="button" onClick={handleSubmitAvatar} className="btn btn-accent">
+                <ShuffleIcon className="size-4 mr-2"/>
+                Generate Random Avatar
+              </button>
 
             </div>
+            {/*FULLNAME*/}
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Full Name</span>
+              </label>
+              <input
+              type="text"
+              name="fullName"
+              value={formState.fullName}
+              onChange={(e)=>setFormState({...formState,fullName:e.target.value})}
+              className="input input-bordered w-full"
+              placeholder="Your full name"
+              />
+            
+            </div>
+            {/*BIO*/}
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Bio</span>
+              </label>
+              <textarea
+              name="bio"
+              value={formState.bio}
+              onChange={(e)=>setFormState({...formState,bio:e.target.value})}
+              className="textarea textarea-bordered h-24"
+              placeholder="Tell others about yourself and your language learning goals" />
+
+            </div>
+           {/*LANGUAGES*/}
+           <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+            {/*Native Language*/}
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Native Language</span>
+             </label>
+             <select
+             name="nativeLanguage"
+             value={formState.nativeLanguage}
+             onChange={(e)=>setFormState({...formState,nativeLanguage:e.target.value})}
+             className="select select-bordered w-full" >
+              <option value="">Select Your native Language</option>
+              {LANGUAGES.map((lang)=>(<option key={`native-${lang}`} value={lang.toLowerCase()}>{lang}</option>))}
+             </select>
+            </div>
+            {/*Learning Language*/}
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Learning Language</span>
+              </label>
+              <select name="learningLanguage"
+              value={formState.learningLanguage}
+              onChange={(e)=>setFormState({...formState,learningLanguage:e.target.value})}
+              className="select select-bordered w-full" >
+                <option value="">Select Your Learning Language</option>
+                {LANGUAGES.map((lang)=>(
+                  <option key={`learning${lang}`} value={lang.toLowerCase()}>{lang} </option>
+                ))}
+              </select>
+            </div>
+           </div>
+            </div>
+            <div className="form-control">
+              <label className="label">
+              <span className="label-text">Location</span>
+              </label>
+              <div className="relative">
+                <MapPinIcon className="absolute top-1/2 transfrom -translate-y-1/2 left-3 size-5 text-base-content opacity-70"/>
+                <input type="text"
+                name="location"
+                value={formState.location}
+                onChange={(e)=>setFormState({...formState,location:e.target.value})}
+                className="input input-bordered w-full pl-10"
+                placeholder="City, Country"/>
+              </div>
+            </div>
+            {/*SUBMIT BUTTON*/}
+            <button className="btn btn-primary w-full" disabled={isPending} type="submit">
+              {!isPending?(<>
+              <ShipWheelIcon className="size-5 mr-2"/>Complete Onboarding
+              </>):(<><LoaderIcon className="animate-spin size-5 mr-2"/>
+              Onboarding</> )}
+            </button>
           </form>
 
         </div>

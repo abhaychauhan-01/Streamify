caa5054d96349bc24dd63c3004a0994ca7c7b8a3
ABHAY PRATAP CHAUHAN <abhaychauhan2906@gmail.com>
2026-03-28 13:50:11 +0530

    front work

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -9,7 +9,7 @@ export const login = async (data) => {
   return response.data;
 };
 export const logout = async () => {
-  const response = await axiosInstance.post("/auth/logout");
+  const response = await axiosInstance.post("/auth/logout"); 
   return response.data;
 };
 export const getAuthUser = async () => {
@@ -20,6 +20,24 @@ export const getAuthUser = async () => {
     return null;
   }
 };
+export async function getUserFriends() {
+  const response=await axiosInstance.get("/users/friends");
+  return response.data;
+  
+}
+export async function getRecommendedUsers() {
+  const response = await axiosInstance.get("/users");
+  return response.data;
+}
+
+export async function getOutgoingFriendReqs() {
+  const response=await axiosInstance.get("/users/outgoing-friend-requests");
+  return response.data;
+}
+export async function sendFriendRequest(userId) {
+  const response =await axiosInstance.get(`/users/friend-request/${userId}`);
+  return response.data;
+}
 export const completeOnboarding =async (userDate)=>{
   const response= await axiosInstance.post("/auth/onboarding",userDate);
   return response.data;
